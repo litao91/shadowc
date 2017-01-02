c4d962c052da3cabb85b79ca217ed7f922a1abd6
TaoLI <litao912002@hotmail.com>
2017-01-02 13:10:14 +0000

    Add tcprelay

--- include/tcprelay.hpp
+++ include/tcprelay.hpp
@@ -1,6 +1,7 @@
 #ifndef SHADOW_C_TCPRELAY_H__
 #define SHADOW_C_TCPRELAY_H__
 #include "eventloop.hpp"
+#include "asyncdns.hpp"
 
 namespace tcprelay {
 class TCPRelayHandler: public eventloop::EventHandler {
@@ -8,5 +9,21 @@ class TCPRelayHandler: public eventloop::EventHandler {
         TCPRelayHandler();
         virtual ~TCPRelayHandler();
 };
+
+
+/**
+ * TCPRelay, server only
+ */
+class TCPRelay: public eventloop::EventHandler {
+    public:
+        TCPRelay(asyncdns::DNSResolver* dns_resolver, const char* server, const char* server_port);
+        void add_to_loop(eventloop::EventLoop* loop);
+        void handle_event(const epoll_event* evt);
+    private:
+        int server_socket;
+        eventloop::EventLoop* loop;
+        bool closed;
+};
+
 }
 #endif
--- lib/tcprelay.cpp
+++ lib/tcprelay.cpp
@@ -1,5 +1,10 @@
 #include "tcprelay.hpp"
-
+#include "utils.hpp"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <iostream>
 
 namespace tcprelay {
     //  as ssserver:
@@ -32,4 +37,70 @@ namespace tcprelay {
     const static char WAIT_STATUS_READWRITING = WAIT_STATUS_READING | WAIT_STATUS_WRITING;
 
     const static int BUF_SIZE = 32 * 1024;
+
+
+    // TCPRelay class
+    TCPRelay::TCPRelay(asyncdns::DNSResolver* dns_resolver, const char* server, const char* server_port) {
+        // init members
+        loop = NULL;
+        closed = false;
+
+        // create server socket
+        addrinfo hints;
+        memset(&hints, 0, sizeof(addrinfo));
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+        
+        addrinfo *result;
+        int r;
+        r = getaddrinfo(server, 
+                server_port, 
+                &hints,
+                &result);
+        if (r != 0) {
+            std::cerr << "Get addr info error:" << gai_strerror(r) << std::endl;
+            exit(1);
+        }
+
+        server_socket = 0;
+        for (addrinfo* rp = result; rp != NULL; rp = rp->ai_next) {
+            server_socket = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+            if (server_socket == -1) {
+                continue;
+            }
+            int val = 1;
+            r = setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(int));
+            if (r < 0) {
+                std::cerr << "setsockopt(SO_REUSEADDR) failed" << std::endl;
+            }
+            r = bind(server_socket, rp->ai_addr, rp->ai_addrlen);
+            if (r == 0) {
+                break;
+            }
+        }
+        freeaddrinfo(result);
+        utils::make_socket_non_blocking(server_socket);
+        r = listen(server_socket, 1024);
+        if (r == -1) {
+            std::cerr << "Error listen" << std::endl;
+            abort();
+        }
+    }
+
+    void TCPRelay::add_to_loop(eventloop::EventLoop* loop) {
+        if (this->loop != NULL) {
+            std::cerr << "Loop already added" << std::endl;
+            return;
+        }
+        if (this->closed) {
+            std::cerr << "Already closed" << std::endl;
+            return;
+        }
+        this->loop = loop;
+        this->loop->add(this->server_socket, EPOLLIN | EPOLLERR, this);
+    }
+
+    void TCPRelay::handle_event(const epoll_event* evt) {
+    }
 }
